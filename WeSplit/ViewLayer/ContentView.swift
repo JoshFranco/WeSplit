ddf808c0d7d6f23c3b90c8e8364ecb25c1442d1d
Josh Franco <jsfranco127@gmail.com>
2020-05-07 13:54:05 -0700

    * Implemented the WeSplit App

--- WeSplit/ViewLayer/ContentView.swift
+++ WeSplit/ViewLayer/ContentView.swift
@@ -9,11 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPpl = 2
+    @State private var tipPercentageIndex = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPpl + 2)
+        let tipSelectoin = Double(tipPercentages[tipPercentageIndex])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelectoin
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    // MARK: - Body
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPpl) {
+                        ForEach(0..<99) { Text("\($0) ppl") }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentageIndex) {
+                        ForEach(0..<tipPercentages.count) { Text("\(self.tipPercentages[$0])%") }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit", displayMode: .large)
+        }
     }
 }
 
+// MARK: - Private Methods
+private extension ContentView {
+    
+}
+
+// MARK: - Previews
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
